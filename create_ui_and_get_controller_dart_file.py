b83934826ed5bece2708d9aa61f243db7eeee81d
Jonathan John <44868912+jonathanjohn47@users.noreply.github.com>
2024-10-28 14:19:30 +0530

    Changes

--- create_ui_and_get_controller_dart_file.py
+++ create_ui_and_get_controller_dart_file.py
@@ -22,7 +22,7 @@ def create_files(folder_path, name):
 
     # Create and open the 'update_request_screen.dart' file
     with open(ui_file_path, 'w') as ui_file:
-        ui_file.write(f'''class {getCamelCase(name)}Screen extends StatelessWidget {{
+        ui_file.write(f'''import 'package:flutter/material.dart';\nclass {getCamelCase(name)}Screen extends StatelessWidget {{
   const {getCamelCase(name)}Screen({{super.key}});
 
   @override
@@ -33,7 +33,8 @@ def create_files(folder_path, name):
 
     # Create and open the 'update_request_get_controller.dart' file
     with open(controller_file_path, 'w') as controller_file:
-        controller_file.write(f'class {getCamelCase(name)}GetController extends GetxController {{}}\n')
+        controller_file.write(
+            f'''import 'package:get/get.dart';\nclass {getCamelCase(name)}GetController extends GetxController {{}}\n''')
 
     print(f"Files '{ui_file_name}' and '{controller_file_name}' were created successfully.")
 
