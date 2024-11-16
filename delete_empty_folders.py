3abb0e0dbc675bc41b1ef827b147e5e0bc179b00
Jonathan John <44868912+jonathanjohn47@users.noreply.github.com>
2024-11-16 07:37:07 +0530

    Changes

--- delete_empty_folders.py
+++ delete_empty_folders.py
@@ -11,6 +11,7 @@ def delete_empty_folders(folder_path):
                 os.rmdir(dir_path)
                 print(f"Deleted empty folder: {dir_path}")
 
+
 def delete_non_git_folders(folder_path):
     for dir_name in os.listdir(folder_path):
         dir_path = os.path.join(folder_path, dir_name)
@@ -19,6 +20,17 @@ def delete_non_git_folders(folder_path):
             if not os.path.exists(git_folder_path):
                 shutil.rmtree(dir_path)
                 print(f"Deleted non-git folder: {dir_path}")
+
+
+def delete_files_with_given_text_in_its_name(folder_path, text):
+    for file_name in os.listdir(folder_path):
+        file_path = os.path.join(folder_path, file_name)
+        if os.path.isfile(file_path) and text in file_name:
+            os.remove(file_path)
+            print(f"Deleted file: {file_path}")
+
+
 if __name__ == "__main__":
     folder_address = input("Enter the folder address: ").strip()
-    delete_non_git_folders(folder_address)
+    text_to_include = input("Enter the text to include in the files: ").strip()
+    delete_files_with_given_text_in_its_name(folder_address, text_to_include)
